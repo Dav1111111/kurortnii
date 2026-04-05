275af5b0e4e08c003cca9836a56690486f3d99ec
Давид <s354005s@gmail.com>
2026-04-06 01:50:24 +0300

    fix: отключить image optimization для кириллических путей

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        pathname: '/**',
-      }
-    ]
+    unoptimized: true,
   }
 };
 
